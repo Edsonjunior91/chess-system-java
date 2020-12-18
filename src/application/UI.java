64c297ed353f065efea3930deb11094eb8cf7013
Edson Junior <edson_junior91@hotmail.com>
2020-12-18 10:29:29 -0300

    Pawn

--- src/application/UI.java
+++ src/application/UI.java
@@ -68,6 +68,7 @@ public class UI {
 		
 	}
 	public static void printBoard(ChessPiece[][] pieces) {
+		System.out.println("  a b c d e f g h");
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
@@ -79,6 +80,7 @@ public class UI {
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		System.out.println("  a b c d e f g h");
 		for(int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i)+ " ");
 			for(int j = 0; j <pieces.length; j++) {
